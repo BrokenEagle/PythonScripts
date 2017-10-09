7806de23a9cc1f5f143bdd529eb2b759b3f4231f
BrokenEagle <BrokenEagle98@yahoo.com>
2017-10-08 20:09:25 -0700

    Updated myglobal configuration

--- lib/myglobal.py
+++ lib/myglobal.py
@@ -6,13 +6,30 @@
 #Other domains include hijiribe, sonohara, and testbooru
 booru_domain = "http://danbooru.donmai.us"
 
-"""Filepaths with backslashes ('\') need to be double escaped ('\\') as demonstrated below"""
+"""Remove the preceding and following triple apostrophes ''' in the Windows or Linux section"""
+
+###WINDOWS###
+'''
+"""Filepaths need to end with a double backslash ('\\')"""
+"""All backslashes ('\') in a filepath need to be double escaped ('\\')"""
 workingdirectory = "D:\\temp\\"
 datafilepath = "data\\"
 csvfilepath = "csv\\"
 jsonfilepath = "json\\"
 dtextfilepath = "dtext\\"
 imagefilepath = "pictures\\"
+'''
+
+###LINUX###
+'''
+"""Filepaths need to end with a forwardslash ('/')"""
+workingdirectory = "/home/USERNAME/temp/"
+datafilepath = "data/"
+csvfilepath = "csv/"
+jsonfilepath = "json/"
+dtextfilepath = "dtext/"
+imagefilepath = "pictures/"
+'''
 
 """Uncomment the following lines (remove the '#') and enter user specific info"""
 #username = "USERNAME_GOES_HERE"
